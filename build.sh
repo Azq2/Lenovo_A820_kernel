9ccde13da232eba54ad13867b13510806a4ce4c5
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2023-01-16 15:24:51 +0200

    use wifi driver from G700 kernel tree

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ DIR=$(readlink -f $(dirname $0))
 # From https://developer.arm.com/downloads/-/gnu-rm
 export CROSS_COMPILE="ccache /opt/gcc-arm-none-eabi-5_3-2016q1/bin/arm-none-eabi-"
 
-export CCACHE_DIR="$DIR/.ccache"
 export ARCH=arm
 export TARGET_PRODUCT=lenovo89_cu_jb
 export TARGET_BUILD_VARIANT=user
@@ -15,12 +14,13 @@ export KBUILD_BUILD_USER=build
 export KBUILD_BUILD_HOST=localhost
 export MTK_ROOT_CUSTOM="$DIR/mediatek/custom/"
 export KERNEL_OUT_DIR="$DIR/out"
+export CCACHE_DIR="$KERNEL_OUT_DIR/ccache"
 export RELEASE_OUT_DIR="$DIR/release"
-export LD_PRELOAD=
+unset LD_PRELOAD
 
-[[ -d $CCACHE_DIR ]] || mkdir $CCACHE_DIR
 [[ -d $KERNEL_OUT_DIR ]] || mkdir $KERNEL_OUT_DIR
 [[ -d $RELEASE_OUT_DIR ]] || mkdir $RELEASE_OUT_DIR
+[[ -d $CCACHE_DIR ]] || mkdir $CCACHE_DIR
 
 echo "CROSS_COMPILE=$CROSS_COMPILE"
 
